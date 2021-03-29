bdaf5f718ff54f344dd781239f86e82253d87113
Chris Matthias <chrism@shorelinemedianj.com>
2021-03-29 14:38:22 -0400

    moved the output of header/footer/body codes to rely on a filter and removed check for explicit sl9_dev; added functionality in shoreline-dev-mode plugin to use the filter to disable output

--- shoreline-scripts-codes.php
+++ shoreline-scripts-codes.php
@@ -3,7 +3,7 @@
 * Plugin Name: 					Scripts and Codes
 * Plugin URI: 					https://github.com/shorelinemedia/shoreline-scripts-codes
 * Description: 					Adds a section in the Customizer to add scripts and codes to header, footer and just after body
-* Version: 							1.0.4
+* Version: 							1.1
 * Author: 							Shoreline Media
 * Author URI: 					https://shoreline.media
 * License:           		GNU General Public License v2
@@ -23,16 +23,17 @@ class SL9_Scripts_Codes {
   // Hold our body hook name
   public $body_hook;
 
-  // Hold our is_dev variable
-  public $is_dev;
+  // Hold our disabled variable
+  public $is_disabled;
 
   public function __construct() {
     $this->namespace = 'sl9_scripts_codes';
-    $this->is_dev = function_exists( 'sl9_is_staging' ) ? sl9_is_staging() : false;
 
     // Get our body hook from the Customizer settings
     // Default is 'wp_body_open'
     $this->body_hook = get_theme_mod('scriptscodes_body_hook', 'wp_body_open');
+
+    add_action( 'plugins_loaded', array( $this, 'init' ) );
 
   }
 
@@ -49,13 +50,16 @@ class SL9_Scripts_Codes {
     // Setup Customizer
     add_action("customize_register", array( $this, 'customize_register' ), 20);
 
-    // Hook our custom scripts/codes into the template
+    // Allow plugins to disable output via filter
+    $this->is_disabled = apply_filters( 'sl9_scripts_codes_disable', false );
+
+    // Allow disabling through filter
+    if ( $this->is_disabled ) { return; }
+
     // Only hook our scripts and codes if we aren't in dev mode
-    if ( !$this->is_dev ) {
-      add_action( 'wp_head', array( $this, 'code_head' ), 300 );
-      add_action( $this->body_hook, array( $this, 'code_body' ), 1 );
-      add_action( 'wp_footer', array( $this, 'code_footer' ), 1 );
-    }
+    add_action( 'wp_head', array( $this, 'code_head' ), 300 );
+    add_action( $this->body_hook, array( $this, 'code_body' ), 1 );
+    add_action( 'wp_footer', array( $this, 'code_footer' ), 1 );
   }
 
   public function customize_register( $wp_customize ) {
@@ -140,4 +144,3 @@ class SL9_Scripts_Codes {
 }
 
 $sl9_scripts_codes = new SL9_Scripts_Codes();
-$sl9_scripts_codes->init();
